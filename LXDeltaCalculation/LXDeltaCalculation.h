ac8f3b651c463d0484357701ce5e93b4c206ed3c
Stan Chang Khin Boon <khinboon@gmail.com>
2015-03-03 03:26:34 +0800

    Initializes an empty project.

---
+++ LXDeltaCalculation/LXDeltaCalculation.h
@@ -0,0 +1,19 @@
+//
+//  LXDeltaCalculation.h
+//  LXDeltaCalculation
+//
+//  Created by Stan Chang Khin Boon on 3/3/15.
+//  Copyright (c) 2015 lxcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LXDeltaCalculation.
+FOUNDATION_EXPORT double LXDeltaCalculationVersionNumber;
+
+//! Project version string for LXDeltaCalculation.
+FOUNDATION_EXPORT const unsigned char LXDeltaCalculationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LXDeltaCalculation/PublicHeader.h>
+
+
